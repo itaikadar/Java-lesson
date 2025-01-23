cefca02c4a0a0dbd1093f8e8b618360da1ace556
itaikadar <itai.kadar@gmail.com>
2025-01-23 17:28:03 +0200

    Homework

--- src/Kadar.java
+++ src/Kadar.java
@@ -2,6 +2,7 @@ public class Kadar {
     public static void main(String[] args) {
         String name="itai kadar";
         System.out.println("my name is " + name);
+        System.out.println("my name is " + name);
     }
 
 }
